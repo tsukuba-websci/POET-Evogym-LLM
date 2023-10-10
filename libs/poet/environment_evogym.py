1894386a1abbb267817fe7e6ae9835c4c009cfe0
Riku <bikkuri19991023@yahoo.co.jp>
2023-10-10 17:49:38 +0900

    LLM用にコードを書き換え

--- libs/poet/environment_evogym.py
+++ libs/poet/environment_evogym.py
@@ -248,17 +248,26 @@ class TerrainParams:
             json.dump(params, f)
 
 
-class EnvironmentEvogym:
-    def __init__(self, key, cppn_genome, terrain_params):
+class EnvironmentEvogym: # CPPNの環境をLLMでの関数に置き換える。
+    def __init__(self, key, LLM_env):
         self.key = key
-        self.cppn_genome = cppn_genome
-        self.terrain_params = terrain_params
+        #self.cppn_genome = cppn_genome
+        #self.terrain_params = terrain_params
         self.terrain = None
+        self.LLM_env = LLM_env # LLMの環境を受け取る。
 
-    def make_terrain(self, decode_function, genome_config):
+    def make_terrain(self, decode_function, genome_config):# 生成した環境をEvogym用に変換する(LLMでは関数でここまで行うので不要)
         terrain = decode_function(self.cppn_genome, genome_config, self.terrain_params)
         self.terrain = terrain
         for platform in terrain['objects'].values():
+            indices = platform['indices']
+            for i,nei in platform['neighbors'].items():
+                for n in nei:
+                    assert n in indices, f'{i}: n'
+    
+    def make_terrain_LLM(self, env):# LLMの環境をEvogym用に変換する。
+        self.terrain = env
+        for platform in env['objects'].values():
             indices = platform['indices']
             for i,nei in platform['neighbors'].items():
                 for n in nei:
@@ -284,7 +293,7 @@ class EnvironmentEvogym:
         terrain_figure = os.path.join(path, 'terrain.jpg')
         self.save_terrain_figure(terrain_figure)
 
-    def save_terrain_figure(self, filename):
+    def save_terrain_figure(self, filename):# 環境の画像を保存する。
         width, height = self.terrain['grid_width'], self.terrain['grid_height']+5
         fig, ax = plt.subplots(figsize=(width/8, height/8))
         for platform in self.terrain['objects'].values():
@@ -304,7 +313,7 @@ class EnvironmentEvogym:
         plt.savefig(filename, bbox_inches='tight')
         plt.close()
 
-    def get_env_info(self, config):
+    def get_env_info(self, config):# 環境の情報を取得する。
 
         env_kwargs = dict(**config.robot, terrain=self.terrain)
 
@@ -315,12 +324,14 @@ class EnvironmentEvogym:
         }
         return make_env_kwargs
 
-    def reproduce(self, config):
+    def reproduce(self, config):#環境の再生成を行う。
         key = config.get_new_env_key()
-        child_cppn = config.reproduce_cppn_genome(self.cppn_genome)
-        child_params = config.reproduce_terrain_params(self.terrain_params)
-        child = EnvironmentEvogym(key, child_cppn, child_params)
-        child.make_terrain(config.decode_cppn, config.neat_config.genome_config)
+        #child_cppn = config.reproduce_cppn_genome(self.cppn_genome)
+        #child_params = config.reproduce_terrain_params(self.terrain_params)
+        child_LLM = LLM_gen() # LLMの環境を受け取る。
+        child = EnvironmentEvogym(key, child_LLM)
+        #child.make_terrain(config.decode_cppn, config.neat_config.genome_config)
+        child.make_terrain_LLM(child_LLM)
         return child
 
 
